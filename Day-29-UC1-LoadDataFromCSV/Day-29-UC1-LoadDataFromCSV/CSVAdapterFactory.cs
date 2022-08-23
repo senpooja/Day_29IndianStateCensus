6110a007c5f95dfdd8e34e1c17462286cc1a03e7
senpooja <poojasen744055@gmail.com>
2022-08-23 19:20:00 +0530

    UC_1Load data from csv files

---
+++ Day-29-UC1-LoadDataFromCSV/Day-29-UC1-LoadDataFromCSV/CSVAdapterFactory.cs
@@ -0,0 +1,25 @@
+﻿using Day_29_UC1_LoadDataFromCSV.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_29_UC1_LoadDataFromCSV
+{
+    class CSVAdapterFactory
+    {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                //case (CensusAnalyser.Country.US):
+                //    return new USCensusAdapter().LoadUSCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
+    }
+}
